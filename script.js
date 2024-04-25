a3ccf6ffd507dcd1b038fe873f240b2d176ea440
nugo jelia <166942516+Duudaa004@users.noreply.github.com>
2024-04-25 15:32:28 +0400

    add script.js file

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// work 1 
+var userName = "Nugo";
+let age = 23;
+const hasCar = true;
+console.log(`Hello, I am ${userName} and I am ${age} years old.`);
+userName = "Duda";
+age = 23;
+console.log(`Hello, I am ${userName} and I am ${age} years old.`);
+
+// work 2
+let x = 4;
+let y = 2;
+let add = x + y;
+console.log(add);
+let difference = x - y;
+console.log(difference);
+let multiply = x * y;
+console.log(multiply);
+let divide = x / y;
+console.log(divide);
+
+// work 3
+let person = 12000;
+let item = 8000;
+console.log("Unfortunately we have only " + item + " sword and we have " + person + " soldiers ");
+console.log(`Unfortunately we have only ${item} sword and we have ${person} soldiers`);
+
+// work 4
+let width = 7;
+let length = 5;
+let area = width * length;
+console.log(area);
+console.log(`the area of this rectangle is ${area}`);
